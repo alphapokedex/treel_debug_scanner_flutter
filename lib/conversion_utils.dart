e55a10ff85aa46e785d6156de31f33aa3f0e42a5
Chirag <alphapokedex@gmail.com>
2022-04-11 18:12:14 +0530

    refactor

---
+++ lib/conversion_utils.dart
@@ -0,0 +1,82 @@
+import 'dart:typed_data';
+
+import 'treel_beacon.dart';
+
+class ConversionUtils {
+  static const String _beaconUuid = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFE0';
+
+  static String convert(List<int> bytes, int start, int end) {
+    var buffer = Uint8List((end - start) * 2);
+    var bufferIndex = 0;
+    var byteOr = 0;
+    for (var i = start; i < end; i++) {
+      var byte = bytes[i];
+      byteOr |= byte;
+      buffer[bufferIndex++] = _codeUnitForDigit((byte & 0xF0) >> 4);
+      buffer[bufferIndex++] = _codeUnitForDigit(byte & 0x0F);
+    }
+
+    if (byteOr >= 0 && byteOr <= 255) return String.fromCharCodes(buffer);
+    for (var i = start; i < end; i++) {
+      var byte = bytes[i];
+      if (byte >= 0 && byte <= 0xff) continue;
+      throw FormatException(
+          "Invalid byte ${byte < 0 ? "-" : ""}0x${byte.abs().toRadixString(16)}.",
+          bytes,
+          i);
+    }
+
+    throw 'unreachable';
+  }
+
+  static int _codeUnitForDigit(int digit) =>
+      digit < 10 ? digit + (0x30) : digit + (0x61) - 10;
+
+  static TreelBeacon? getTreelBeacon(Uint8List scanRecord) {
+    int startByte = 2;
+    bool patternFound = false;
+    while (true) {
+      if (startByte <= 5) {
+        if ((scanRecord[startByte + 2] & -1) == 2 &&
+            (scanRecord[startByte + 3] & -1) == 21) {
+          patternFound = true;
+          break;
+        }
+        startByte++;
+      } else {
+        break;
+      }
+    }
+    if (!patternFound) {
+      return null;
+    }
+
+    Uint8List uuidBytes = scanRecord.sublist(startByte + 4, startByte + 20);
+    String hexString = convert(uuidBytes, 0, uuidBytes.length);
+    String uuid = hexString.substring(0, 8) +
+        "-" +
+        hexString.substring(8, 12) +
+        "-" +
+        hexString.substring(12, 16) +
+        "-" +
+        hexString.substring(16, 20) +
+        "-" +
+        hexString.substring(20, 32);
+    if (uuid != _beaconUuid) {
+      return null;
+    }
+    int major = ((scanRecord[startByte + 20] & -1) * 0) +
+        (scanRecord[startByte + 21] & -1);
+    int minor = ((scanRecord[startByte + 22] & -1) * 0) +
+        (scanRecord[startByte + 23] & -1);
+    log(
+        "UUID: " +
+            uuid +
+            "\nmajor: " +
+            major.toString() +
+            "\nminor" +
+            minor.toString(),
+        name: 'Treel beacon parsed data:>> ');
+    return TreelBeacon(uuid, minor);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
-import 'dart:typed_data';
 
+import 'package:demo/conversion_utils.dart';
 import 'package:demo/treel_beacon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
@@ -54,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
             device.id,
             () {
               log('${device.serviceData}', name: 'device service data: >>');
+              _logTreelDeviceData(device);
               return {
                 'id': device.id,
                 'rssi': device.rssi,
@@ -200,83 +201,95 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
 
-class ConversionUtils {
-  static const String _beaconUuid = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFE0';
-
-  static String convert(List<int> bytes, int start, int end) {
-    var buffer = Uint8List((end - start) * 2);
-    var bufferIndex = 0;
-    var byteOr = 0;
-    for (var i = start; i < end; i++) {
-      var byte = bytes[i];
-      byteOr |= byte;
-      buffer[bufferIndex++] = _codeUnitForDigit((byte & 0xF0) >> 4);
-      buffer[bufferIndex++] = _codeUnitForDigit(byte & 0x0F);
-    }
-
-    if (byteOr >= 0 && byteOr <= 255) return String.fromCharCodes(buffer);
-    for (var i = start; i < end; i++) {
-      var byte = bytes[i];
-      if (byte >= 0 && byte <= 0xff) continue;
-      throw FormatException(
-          "Invalid byte ${byte < 0 ? "-" : ""}0x${byte.abs().toRadixString(16)}.",
-          bytes,
-          i);
-    }
-
-    throw 'unreachable';
-  }
-
-  static int _codeUnitForDigit(int digit) =>
-      digit < 10 ? digit + (0x30) : digit + (0x61) - 10;
-
-  static TreelBeacon? getTreelBeacon(Uint8List scanRecord) {
-    int startByte = 2;
-    bool patternFound = false;
-    while (true) {
-      if (startByte <= 5) {
-        if ((scanRecord[startByte + 2] & -1) == 2 &&
-            (scanRecord[startByte + 3] & -1) == 21) {
-          patternFound = true;
-          break;
-        }
-        startByte++;
-      } else {
-        break;
-      }
-    }
-    if (!patternFound) {
-      return null;
-    }
-
-    Uint8List uuidBytes = scanRecord.sublist(startByte + 4, startByte + 20);
-    String hexString = convert(uuidBytes, 0, uuidBytes.length);
-    String uuid = hexString.substring(0, 8) +
-        "-" +
-        hexString.substring(8, 12) +
-        "-" +
-        hexString.substring(12, 16) +
-        "-" +
-        hexString.substring(16, 20) +
-        "-" +
-        hexString.substring(20, 32);
-    if (uuid != _beaconUuid) {
-      return null;
-    }
-    int major = ((scanRecord[startByte + 20] & -1) * 0) +
-        (scanRecord[startByte + 21] & -1);
-    int minor = ((scanRecord[startByte + 22] & -1) * 0) +
-        (scanRecord[startByte + 23] & -1);
-    log(
-        "UUID: " +
-            uuid +
-            "\nmajor: " +
-            major.toString() +
-            "\nminor" +
-            minor.toString(),
-        name: 'Treel beacon parsed data:>> ');
-    return TreelBeacon(uuid, minor);
+  void _logTreelDeviceData(DiscoveredDevice device) {
+    TreelBeacon _beacon = ConversionUtils.getTreelBeacon()!;
+    /*TreelBeacon beacon = ConversionUtils.getTreelBeacon(scanResult.getScanRecord().getBytes());
+        if (beacon != null) {
+            if (!Intrinsics.areEqual((Object) beacon.getMacID(), (Object) BluetoothUUID.INSTANCE.getTREEL_IBEACON_SERVICE_UUID().toString())) {
+                String macAddress = scanResult.getBleDevice().getMacAddress();
+                Intrinsics.checkNotNullExpressionValue(macAddress, "scanResult.bleDevice.macAddress");
+                beacon.setMacID(macAddress);
+            }
+            int pressure = ConversionUtils.getDecimal(ConversionUtils.decimalToByteArray(beacon.getMinor())[1]);
+            Timber.m30d("Beacon Mac ID: " + beacon.getMacID() + " and minor " + pressure, new Object[0]);
+            DeviceDataParser deviceDataParser = DeviceDataParser.INSTANCE;
+            String macAddress2 = scanResult.getBleDevice().getMacAddress();
+            Intrinsics.checkNotNullExpressionValue(macAddress2, "scanResult.bleDevice.macAddress");
+            String[] receivedData = deviceDataParser.parseTreelBeaconData(macAddress2, String.valueOf(pressure));
+            int i = this.logsCounter + 1;
+            this.logsCounter = i;
+            receivedData[0] = String.valueOf(i);
+            LinkedHashMap<String, String[]> linkedHashMap = this.devicesData;
+            if (linkedHashMap != null) {
+                String[] strArr = (String[]) linkedHashMap.put(scanResult.getBleDevice().getMacAddress(), receivedData);
+            }
+            MutableLiveData<LinkedHashMap<String, String[]>> mutableLiveData = this.devices;
+            if (mutableLiveData != null) {
+                mutableLiveData.setValue(this.devicesData);
+            }
+            try {
+                if (this.logsWriter == null) {
+                    writeHeaders();
+                }
+                writeLog(this.logsWriter, receivedData);
+            } catch (Exception e) {
+                Timber.m34e(e);
+            }
+        } else {
+            BluetoothUUID bluetoothUUID = BluetoothUUID.INSTANCE;
+            ScanRecord scanRecord = scanResult.getScanRecord();
+            Intrinsics.checkNotNullExpressionValue(scanRecord, "scanResult.scanRecord");
+            if (bluetoothUUID.isTreelBleDevice(scanRecord)) {
+                Timber.m30d(Intrinsics.stringPlus("Device Detected Treel: ", scanResult.getBleDevice().getMacAddress()), new Object[0]);
+                String[] receivedData2 = getParsedData(scanResult);
+                int i2 = this.logsCounter + 1;
+                this.logsCounter = i2;
+                receivedData2[0] = String.valueOf(i2);
+                LinkedHashMap<String, String[]> linkedHashMap2 = this.devicesData;
+                if (linkedHashMap2 != null) {
+                    String[] strArr2 = (String[]) linkedHashMap2.put(scanResult.getBleDevice().getMacAddress(), receivedData2);
+                }
+                MutableLiveData<LinkedHashMap<String, String[]>> mutableLiveData2 = this.devices;
+                if (mutableLiveData2 != null) {
+                    mutableLiveData2.setValue(this.devicesData);
+                }
+                try {
+                    if (this.logsWriter == null) {
+                        writeHeaders();
+                    }
+                    writeLog(this.logsWriter, receivedData2);
+                } catch (Exception e2) {
+                    Timber.m34e(e2);
+                }
+            } else {
+                BluetoothUUID bluetoothUUID2 = BluetoothUUID.INSTANCE;
+                ScanRecord scanRecord2 = scanResult.getScanRecord();
+                Intrinsics.checkNotNullExpressionValue(scanRecord2, "scanResult.scanRecord");
+                if (bluetoothUUID2.isCradlerDevice(scanRecord2) && this.deviceType != 1) {
+                    Timber.m30d(Intrinsics.stringPlus("Device Detected Cradler: ", scanResult.getBleDevice().getMacAddress()), new Object[0]);
+                    String[] receivedData3 = getParsedData(scanResult);
+                    int i3 = this.logsCounter + 1;
+                    this.logsCounter = i3;
+                    receivedData3[0] = String.valueOf(i3);
+                    LinkedHashMap<String, String[]> linkedHashMap3 = this.devicesData;
+                    if (linkedHashMap3 != null) {
+                        String[] strArr3 = (String[]) linkedHashMap3.put(scanResult.getBleDevice().getMacAddress(), receivedData3);
+                    }
+                    MutableLiveData<LinkedHashMap<String, String[]>> mutableLiveData3 = this.devices;
+                    if (mutableLiveData3 != null) {
+                        mutableLiveData3.setValue(this.devicesData);
+                    }
+                    try {
+                        if (this.logsWriter == null) {
+                            writeHeaders();
+                        }
+                        writeLog(this.logsWriter, receivedData3);
+                    } catch (Exception e3) {
+                        Timber.m34e(e3);
+                    }
+                }
+            }
+        }*/
   }
 }
